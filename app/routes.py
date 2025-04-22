33e04e3eea1407ed45412a3034da1e4e3b3700a0
Lanaiya Wu <legaspi.lanaiya@gmail.com>
2025-04-21 23:02:16 -0700

    added links to Navbar - Recipes, My Grocery List; created template for use case #6. View Recipe

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,14 @@ from app import myapp_obj
 def main():
 	return render_template("index.html")
 
+@myapp_obj.route("/recipes")
+def recipes():
+	return render_template("recipes.html")
+
+@myapp_obj.route("/recipe")
+def recipeX():
+	return render_template("recipe.html")
+
 @myapp_obj.route("/grocery-list")
 def grocery_list():
 	return render_template("grocery-list.html")
