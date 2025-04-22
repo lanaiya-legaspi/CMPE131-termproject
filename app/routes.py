ca46a67e21b25edee728609c7c3db2e98915ff7d
Lanaiya Wu <legaspi.lanaiya@gmail.com>
2025-04-21 22:27:52 -0700

    added files to create templates for home page and grocery list page

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+from app import myapp_obj
+
+@myapp_obj.route("/")
+@myapp_obj.route("/home")
+def main():
+	return render_template("index.html")
+
+@myapp_obj.route("/grocery-list")
+def grocery_list():
+	return render_template("grocery-list.html")
